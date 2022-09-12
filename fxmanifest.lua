fd522e5641656058690ed19c8708b3eb6f74b2ab
NaorNC <81817039+NaorNC@users.noreply.github.com>
2022-09-12 03:16:09 +0300

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,19 @@
+-- #Discord.gg/NCHub
+fx_version 'cerulean'
+game 'gta5'
+ 
+files {
+    'handling.meta',
+    'vehiclelayouts.meta',
+    'vehicles.meta',
+	'carcols.meta',
+    'carvariations.meta',
+	'vehiclemodelsets.meta',
+}
+
+data_file 'HANDLING_FILE' 'handling.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'vehiclelayouts.meta'
+data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
+data_file 'CARCOLS_FILE' 'carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
+data_file 'VEHICLE_MODEL_SETS_FILE' 'vehiclemodelsets.meta'
